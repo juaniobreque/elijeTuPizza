d61c0ef4e8eae5caad65a538e8df5ee54bd6d7c7
Juan Ignacio Obreque <91294886+juaniobreque@users.noreply.github.com>
2022-06-18 18:53:34 -0300

    Update app.js

--- javascript_e3/app.js
+++ javascript_e3/app.js
@@ -2,14 +2,26 @@ import { consultarBD } from "./consulta.js"
 import { pizzas } from "./pizzas.js"
 import { mostrarError } from "./error.js"
 
+// Variables
 const form = document.getElementById('form')
 
 window.addEventListener('load', () => {
     const strPizzas = JSON.stringify(pizzas)
     localStorage.setItem('pizzas', strPizzas)
-    
-    form.addEventListener('submit', buscarPizza)
+    form.addEventListener("mouseover", cambioBoton)
+    form.addEventListener("mouseout", vueltaBoton)
+    form.addEventListener('submit', buscarPizza)    
 })
+
+
+function cambioBoton(){
+    event.target.style.color = "#e7b993";
+}
+function vueltaBoton(){
+    event.target.style.color = "";
+}
+
+
 
 function buscarPizza(e) {
     e.preventDefault();
